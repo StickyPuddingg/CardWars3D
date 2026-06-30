2097ab5e144903faef5f0643a6ac234028bb5e45
Jalm24 <jalm24032003@gmail.com>
2026-06-30 00:32:07 -0400

    I assigned an AI to remove the network components and it seems to work

---
+++ Assets/Editor/WiiUTitleIdGenerator.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class WiiUTitleIdGeneratorWindow : EditorWindow
+{
+    public enum WiiURegion
+    {
+        USA = 0x00,
+        EUR = 0x01,
+        JPN = 0x02
+    }
+
+    private bool isBaseGame = true;
+    private WiiURegion region = WiiURegion.USA;
+    private string generatedTitleId = "";
+
+    // Añade una opción en el menú superior de Unity: Window > Wii U Title ID Generator
+    [MenuItem("Window/Wii U Title ID Generator")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(WiiUTitleIdGeneratorWindow), false, "Wii U ID Gen");
+    }
+
+    // Dibuja la interfaz de la ventana flotante
+    private void OnGUI()
+    {
+        GUILayout.Label("Configuración de Title ID (Patrón Retail)", EditorStyles.boldLabel);
+
+        // Opciones de configuración
+        isBaseGame = EditorGUILayout.Toggle("¿Es Juego Base?", isBaseGame);
+        region = (WiiURegion)EditorGUILayout.EnumPopup("Región:", region);
+
+        GUILayout.Space(10);
+
+        // Botón para ejecutar la lógica
+        if (GUILayout.Button("Generar Title ID"))
+        {
+            generatedTitleId = GenerateRandomTitleID(isBaseGame, region);
+        }
+
+        GUILayout.Space(15);
+
+        // Muestra el resultado si ya se generó uno
+        if (!string.IsNullOrEmpty(generatedTitleId))
+        {
+            EditorGUILayout.TextField("Title ID Generado:", generatedTitleId);
+
+            if (GUILayout.Button("Copiar al Portapapeles"))
+            {
+                EditorGUIUtility.systemCopyBuffer = generatedTitleId;
+                Debug.Log(string.Format("[Wii U] Copiado al portapapeles: {0}", generatedTitleId));
+            }
+        }
+    }
+
+    private static string GenerateRandomTitleID(bool isBase, WiiURegion reg)
+    {
+        // 1. Primer Bloque (High Title ID)
+        string highTitleId = isBase ? "00050000" : "0005000c";
+
+        // 2. Bloque Intermedio Fijo (Patrón Comercial/Retail)
+        string segmentoretail = "104";
+
+        // 3. ID Único del Juego (3 dígitos hexadecimales aleatorios: 000 a FFF)
+        // Rango en decimal: 0 a 4095 (0xFFF). En Random.Range el máximo es exclusivo, por eso 4096.
+        int randomProductNum = Random.Range(0, 4096);
+        string idUnicoJuego = randomProductNum.ToString("X3");
+
+        // 4. Código de Región (Últimos 2 dígitos)
+        string regionId = ((int)reg).ToString("X2");
+
+        // Combinamos todo respetando la estructura real que observaste
+        return (highTitleId + segmentoretail + idUnicoJuego + regionId).ToLower();
+    }
+}
---
+++ README.md
@@ -0,0 +1,32 @@
+# Card Wars
+A port of the "Adventure Time: Card Wars" mobile game to PC.
+
+Floop the Pig! It's Adventure Time CARD WARS! Play the game inspired by the Adventure TIme episode, 'Card Wars'! Summon creatures and cast spells to battle your way to victory.
+
+
+CARD COMBAT!
+
+Command an army of awesome warriors, including Husker Knights, Cool Dog the Immortal Maize Walker, and even the Pig to destroy your opponent's forces! Place towers and cast spells to unleash ultimo attacks
+
+CUSTOM DECKS!
+
+Collect new cards to customize your deck for each opponent. Level up your creatures, spells, and towers, or fuse them together to make your cards even more powerful.
+
+HIGH STAKES BATTLES!
+
+Think you've got what it takes to be crowned a Cool Guy, or will you end up drinking from the Dweeb cup? Play as Finn Jake, BMO, Princess Bubblegum, Marceline, Flame Princess and more as you wind your way through the Land of Ooo!
+
+It's CARD WARS! 
+
+## Download
+
+* [Latest Windows Version](https://github.com/shishkabob27/CardWars/releases/latest/download/CardWars-Windows.zip)
+* Mac Version - Coming Soon
+* Linux Version - Coming Soon
+
+## Images
+![CardWars_r8d9H393Tp](https://i.imgur.com/cXUolY0.jpg)
+![CardWars_F7nDRbIxel](https://i.imgur.com/N3BH326.jpg)
+
+## Contributing
+Card Wars uses Unity 2017.4.40f1. No other dependencies are required.
